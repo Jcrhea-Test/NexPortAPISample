2a362b376d5c2fbc56432cd88b4c32a2d6eaaedb
Justin Rhea <jrhea@nexient.com>
2022-09-06 08:56:29 -0400

    Added Get SingleEvent API test

--- NexPortTestProject/GetTests/GetEventsTests.cs
+++ NexPortTestProject/GetTests/GetEventsTests.cs
@@ -26,14 +26,30 @@ namespace NexPortTestProject.GetTests
             string authToken = nexportAPIHelper.getAuthToken();
             RestResponse getEventResponse = new NexPortAPIHelper().NexportGetAPI(testEventInfo.GetEventsUrl, authToken, testEventInfo.VersionNumber);
             JArray listResponse = JArray.Parse(getEventResponse.Content);
-            JToken testResponse = listResponse.Where(responseItem => (string)responseItem["id"] == "ivqmld").FirstOrDefault();
-            JToken attendant = testResponse["attendees"].Where(attendeeID => (string)attendeeID["id"] == "lyshnu").FirstOrDefault();
+            JToken eventResponse = listResponse.Where(responseItem => (string)responseItem["id"] == "ivqmld").FirstOrDefault();
             //Assert
             Assert.That(getEventResponse, Is.Not.Null);
             Assert.That((int)getEventResponse.StatusCode, Is.EqualTo(200));
-            Assert.That(testResponse["title"].ToString(), Is.EqualTo("Scrum Training"));
+            Assert.That(eventResponse["title"].ToString(), Is.EqualTo("Scrum Training"));
+        }
+        // A pass test for Get Single Event API call
+        [Test]
+        public void GetSingleEventTestPass()
+        {
+            //Arrange
+            DataHelpers dataHelper = new DataHelpers();
+            NexPortAPIHelper nexportAPIHelper = new NexPortAPIHelper();
+            GetRequestList.GetEventsRequest testEventInfo = dataHelper.NexportGetEventsInfo();
+            testEventInfo.GetEventsUrl = testEventInfo.GetEventsUrl + "/ivqmld";
+            //Act
+            string authToken = nexportAPIHelper.getAuthToken();
+            RestResponse getEventResponse = new NexPortAPIHelper().NexportGetAPI(testEventInfo.GetEventsUrl, authToken, testEventInfo.VersionNumber);
+            JObject getSingleEventResponse = JObject.Parse(getEventResponse.Content);
+            JToken attendant = getSingleEventResponse["attendees"].Where(attendeeID => (string)attendeeID["id"] == "lyshnu").FirstOrDefault();
+            //Assert
+            Assert.That(getEventResponse, Is.Not.Null);
+            Assert.That((int)getEventResponse.StatusCode, Is.EqualTo(200));
             Assert.That(attendant["name"].ToString(), Is.EqualTo("Justin Rhea"));
-
         }
     }
 }
